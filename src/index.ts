db7e653f42c8b6a93ad3b59a90172691bea41c51
Gonçalo Santos <gregueiras@gmail.com>
2019-09-06 18:04:32 +0100

    refactor client structure

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,24 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 const interval = 30;
 
-const clients = {}; //token is key, value is intervalID
+const clients = {}; //Provider_StopCode is key, [{token, lines: [{line, expire}]}] is value
 
 interface request {
   token: string;
   expire: string;
   stopCode: string;
   provider: string;
-  lines: string[];
+  line: string;
+}
+
+interface line {
+  line: string;
+  expire: number;
+}
+
+interface clientEntry {
+  token: string;
+  lines: line[];
 }
 
 app.post("/unsubscribe", (req, res) => {
@@ -30,34 +40,12 @@ app.post("/unsubscribe", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-
   if (req.body.token) {
-    const {
-      token,
-      expire: expireTemp,
-      stopCode,
-      provider,
-      lines
-    } = req.body as request;
+    const { token, stopCode, provider, line } = req.body as request;
 
-    const expire = expireTemp
-      ? parseInt(String(expireTemp).replace("*", ""))
-      : 10;
+    console.log(`Line ${provider}:${stopCode} requested`);
 
-    console.log(
-      `Dispatching updates for ${provider}:${stopCode} [${lines}] every ${interval} seconds for ${expire} minutes`
-    );
-    const intervalID = setInterval(
-      () => handleReq(token, stopCode, provider, lines),
-      interval * 1000
-    );
-
-    if (clients[token]) clients[token] = [...clients[token], intervalID];
-    else clients[token] = intervalID;
-
-    setTimeout(() => clearInterval(intervalID), expire * 60 * 1000);
+    addClient({ token, provider, stopCode, line });
 
     res.send("SUCESS");
     return;
@@ -67,6 +55,28 @@ app.post("/", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function getCode(provider, stopCode) {
+  return `${provider}_${stopCode}`;
+}
+
+function addClient({ token, provider, stopCode, line }) {
+  const newEntry: clientEntry = {
+    token,
+    lines: [{ line, expire: Infinity }]
+  };
+  
+  const code = getCode(provider, stopCode);
+  const stop = clients[code];
+  
+  if (stop) {
+    clients[code] = [...stop, newEntry]
+  } else {
+    clients[code] = [newEntry]
+  }
+
+  console.log(clients);
+}
 
 async function handleReq(
   token: string,
